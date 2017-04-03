abf3b2ece74836c4d5e2a2916fd235b893651941
Jason <Jasonfreedman1992@gmail.com>
2017-04-03 15:28:33 -0700

    testing socket ip string

--- Server.java
+++ Server.java
@@ -2,22 +2,25 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
+import java.util.*;
+
 
 public class Server
 {
 	public static void main(String[] args) throws IOException
 	{
 		ServerSocket listener = new ServerSocket(49152);
+		ArrayList<Socket> socketList = new ArrayList<Socket>();
 		try
 		{
 			while(true)
 			{
 				Socket socket = listener.accept();
+				
 				try
 				{
 					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-					out.println("test it yo");
+					out.println(socket.getRemoteSocketAddress().toString());
 				}
 				finally
 				{
