a97f222ada939de17d290aa93723f3d78406d305
Jason <Jasonfreedman1992@gmail.com>
2017-04-04 00:18:26 -0700

    changes

--- Server.java
+++ Server.java
@@ -28,7 +28,9 @@ public class Server
 				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 				BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-				//out.println(socket.getRemoteSocketAddress().toString());
+				// send to client
+				out.println(socket.getRemoteSocketAddress().toString());
+
 				System.out.println(input);
 				System.out.println(socket.getRemoteSocketAddress());
 				System.out.println("festival");
