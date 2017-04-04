4d134ab4e277522b808227e8dd6883b7ffaaac15
Jason <Jasonfreedman1992@gmail.com>
2017-04-03 23:37:44 -0700

    changes

--- Server.java
+++ Server.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
@@ -9,6 +11,7 @@ public class Server
 {
 	public static void main(String[] args) throws IOException
 	{
+		Scanner console = new Scanner(System.in);
 		ServerSocket listener = new ServerSocket(49152);
 		ArrayList<Socket> socketList = new ArrayList<Socket>();
 		String testUser = "jason";
@@ -19,16 +22,18 @@ public class Server
 			while(true)
 			{
 				Socket socket = listener.accept();
+				socketList.add(socket);
 
 				try
 				{
 					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+					BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 					out.println(socket.getRemoteSocketAddress().toString());
 					System.out.println(socket.getRemoteSocketAddress());
 				}
 				finally
 				{
-					//socket.close();
+
 				}
 				System.out.println("festival");
 			}
@@ -36,7 +41,6 @@ public class Server
 		finally
 		{
 			listener.close();
-			System.out.println("listener closed");
 		}
 		//System.out.println("Hello World");
 	}
