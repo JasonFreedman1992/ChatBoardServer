abcfa20c82965e610d2dcb3509fcd2f3699867e7
jason <Jasonfreedman1992@gmail.com>
2017-04-04 17:19:43 -0700

    testing multi threading for server client communication

--- ServerProcess.java
+++ ServerProcess.java
@@ -8,48 +8,69 @@ import java.io.*;
 
 public class ServerProcess
 {
+	public String line = "";
 	public Scanner console = new Scanner(System.in);
 	public DataInputStream streamIn = null;
 	public DataOutputStream streamOut = null;
 	public ArrayList<Socket> socketList = new ArrayList<Socket>();
-	public String testUser = "jason";
-	public String testPass = "231";
-	public ServerProcess(Socket p_socket)
-	{
-		//Socket socket = listener.accept();
-		while(true)
-		{
-			try 
-			{
-				System.out.println(p_socket.getRemoteSocketAddress());
-				System.out.println("festival");
-				streamIn = new DataInputStream(new BufferedInputStream(p_socket.getInputStream()));
-				streamOut = new DataOutputStream(p_socket.getOutputStream());
-				boolean hold = true;
-				while(hold)
-				{
-					try
-					{
-						String line = streamIn.readUTF();
-						System.out.println(line);
-						if(line == ".bye")
-						{
-							hold = false;
-						}
-						String output = console.nextLine();
-						streamOut.writeUTF(output);
-						streamOut.flush();
-					}
-					catch(IOException e)
-					{
-						hold = false;
-					}
-				}
-	        } 
-	        catch(IOException e)
-	        {
+	Thread outThread = new Thread();
+	Thread inThread = new Thread();
 
-	        }
-    	}
+	output output = new output();
+	input input = new input();
+
+	public ServerProcess(Socket p_socket) throws IOException
+	{
+		streamOut = new DataOutputStream(p_socket.getOutputStream());
+		streamIn = new DataInputStream(new BufferedInputStream(p_socket.getInputStream()));
+	    try
+	    {
+	    	outThread = new Thread(output);
+	    	outThread.start();
+
+	    	inThread = new Thread(input);
+	    	inThread.start();
+	    }
+	    catch(Exception e)
+	    {
+
+	    }
+	}
+	class output implements Runnable
+	{
+		public void run()
+		{
+			while(true)
+			{
+				try
+				{
+					streamOut.writeUTF(console.nextLine());
+	        		streamOut.flush();
+	        	}
+	        	catch(IOException e)
+	        	{
+	        		break;
+	        	}
+			}
+		}
+	}
+
+	class input implements Runnable
+	{
+		public void run()
+		{
+			while(true)
+			{
+				try
+				{
+					String input = streamIn.readUTF();
+	            	System.out.println(input);
+				}
+	            catch(IOException e)
+	            {
+	            	break;
+	            }
+			}
+		}
 	}
 }
