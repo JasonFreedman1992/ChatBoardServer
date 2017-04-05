16226d774f231f50aa3e6b50ccc4c21eb0c8b14f
jason <jasonfreedman1992@gmail.com>
2017-04-04 22:12:19 -0700

    changes

--- ServerProcess.java
+++ ServerProcess.java
@@ -90,7 +90,7 @@ public class ServerProcess
 		{
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ChatBoard", "ubuntu", "313m3n7!");
 			Statement statement = conn.createStatement();
-			String query = "SELECT * FROM Accounts";
+			String query = "SELECT * FROM Accounts;";
 			ResultSet rs = statement.executeQuery(query);
 			System.out.println(rs);
 		}
