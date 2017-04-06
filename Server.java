47e92fd1d83b23df20cdb5244fddec409595af1b
jason <jasonfreedman1992@gmail.com>
2017-04-06 13:22:38 -0700

    redoing server

--- Server.java
+++ Server.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.io.*;
 
 public class Server
 {
--- ServerInitMaster.java
+++ ServerInitMaster.java
@@ -1,42 +1,57 @@
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.io.IOException;
 import java.util.*;
-import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.net.*;
 
 public class ServerInitMaster
 {
-	public ServerData serverData = new ServerData();
-	private ServerSocket listener;
 	public listen listen = new listen();
-	public Socket next;
-
+	public int port;
+	public ServerSocketChannel initChannel;
 	public ServerInitMaster(int p_port) throws IOException
 	{
-		listener = new ServerSocket(p_port);
+		port = p_port;
 	}
 
 	class listen implements Runnable
 	{
 		public void run()
 		{
-			while(true)
+			try
 			{
-				try
+				initChannel = ServerSocketChannel.open();
+				initChannel.socket().bind(new InetSocketAddress(port));
+				while(true)
 				{
 					System.out.println("listening...");
-					next = listener.accept();
-					System.out.println("initialized... ");
-					if(next.isBound())
-					{
-						System.out.println(next.getRemoteSocketAddress());
-					}
-				}
-				catch(IOException e)
-				{
-					System.out.println(e);
+					initChannel.accept();
+					System.out.println("initialized...");
+					System.out.println(initChannel.socket().getInetAddress());
+
 				}
 			}
+			catch(IOException e)
+			{
+				
+			}
+			// while(true)
+			// {
+			// 	try
+			// 	{
+			// 		System.out.println("listening...");
+			// 		next = listener.accept();
+			// 		System.out.println("initialized... ");
+			// 		if(next.isBound())
+			// 		{
+			// 			System.out.println(next.getRemoteSocketAddress());
+			// 		}
+			// 	}
+			// 	catch(IOException e)
+			// 	{
+			// 		System.out.println(e);
+			// 	}
+			// }
 		}
 	}
 }
