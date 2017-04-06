76e2e349fe0dc3f47e8e5917ebfbe5ea98233be8
jason <jasonfreedman1992@gmail.com>
2017-04-06 00:34:25 -0700

    changes

--- ServerSoftLayerFilter.java
+++ ServerSoftLayerFilter.java
@@ -13,18 +13,25 @@ public class ServerSoftLayerFilter
 		{
 			while(true)
 			{
-				for(int i = 0; i < serverData.softLogins.size(); i++)
-				{	
-					try
-					{
-						streamOut = new DataOutputStream(serverData.softLogins.get(i).getOutputStream());
-						streamOut.writeUTF("sending packet");
-						streamOut.flush();
-					}
-					catch(IOException e)
-					{
-						System.out.println(e);
-						serverData.softLogins.remove(i);
+				if(serverData.softLogins.isEmpty())
+				{
+
+				}
+				else if(!serverData.softLogins.isEmpty())
+				{
+					for(int i = 0; i < serverData.softLogins.size(); i++)
+					{	
+						try
+						{
+							streamOut = new DataOutputStream(serverData.softLogins.get(i).getOutputStream());
+							streamOut.writeUTF("sending packet");
+							streamOut.flush();
+						}
+						catch(IOException e)
+						{
+							System.out.println(e);
+							serverData.softLogins.remove(i);
+						}
 					}
 				}
 			}
