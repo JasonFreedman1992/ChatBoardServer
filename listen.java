f2f1c3e5a291425c2d63097bf179b261558effb5
jason <jasonfreedman1992@gmail.com>
2017-04-06 21:27:32 -0700

    changes

--- listen.java
+++ listen.java
@@ -60,48 +60,48 @@ public class listen implements Runnable
 		catch(IOException e)
 		{
 			System.out.println(" IOException, server of port 49152 terminating, stack trace: " + e);
-			try
-			{
-				mainLoop();
-			}
-			catch(Exception f)
-			{
-				
-			}
+			Thread.currentThread().run();
 		}
 	}
 
 	void mainLoop() throws IOException
 	{
-		Iterator<SelectionKey> iter;
-		SelectionKey key;
-		while(initChannellisten.isOpen())
+		try
 		{
-			selector.select();
-			iter = selector.selectedKeys().iterator();
-			while(iter.hasNext())
+			Iterator<SelectionKey> iter;
+			SelectionKey key;
+			while(initChannellisten.isOpen())
 			{
-				key = iter.next();
-				iter.remove();
-				if(key.isAcceptable())
+				selector.select();
+				iter = selector.selectedKeys().iterator();
+				while(iter.hasNext())
 				{
-					System.out.println("is acceptable");
-					handleAccept(key);
-				}
-				else
-				{
-					// will go here if nothing is coming in
-				}
-				if(key.isReadable())
-				{
-					handleRead(key);
-					// will go here if nothing is coming in
-				}
-				else
-				{
-					System.out.println("not readable");
+					key = iter.next();
+					iter.remove();
+					if(key.isAcceptable())
+					{
+						System.out.println("is acceptable");
+						handleAccept(key);
+					}
+					else
+					{
+						// will go here if nothing is coming in
+					}
+					if(key.isReadable())
+					{
+						handleRead(key);
+						// will go here if nothing is coming in
+					}
+					else
+					{
+						System.out.println("not readable");
+					}
 				}
 			}
+		}
+		catch(IOException e)
+		{
+			System.out.println(" IOException, server of port 49152 terminating, stack trace: " + e);
 		}
 	}
 
