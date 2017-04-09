79affe0faea72e40084ec1751d632a727feffbd1
jason <jasonfreedman1992@gmail.com>
2017-04-08 18:31:04 -0700

    changes

--- listen.java
+++ listen.java
@@ -3,6 +3,7 @@ import java.util.*;
 import java.nio.*;
 import java.nio.channels.*;
 import java.net.*;
+import java.sql.*;
 
 public class listen implements Runnable
 {
@@ -134,6 +135,38 @@ public class listen implements Runnable
 				sch.write(msgBuffer);
 				msgBuffer.rewind();
 			}
+		}
+	}
+	void query()
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ChatBoard?useSSL=false", "root", "313m3n7!");
+			Statement statement = conn.createStatement();
+			String query = "SELECT * FROM Accounts";
+
+			ResultSet rs = statement.executeQuery(query);
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+			while(rs.next())
+			{
+				for(int i = 1; i <= columnsNumber; i++)
+				{
+					String columnValue = rs.getString(i);
+					String columnName = rsmd.getColumnName(i);
+					System.out.println(columnValue + " " + columnName);
+				}
+				System.out.println("");
+			}
+		}
+		catch(SQLException e)
+		{
+			System.out.println("no connection");
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("not found class");
 		}
 	}
 }
