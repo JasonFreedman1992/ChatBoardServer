3841d8d65420798986f5cd48f5bcabcb24ce80f8
jason <jasonfreedman1992@gmail.com>
2017-04-11 15:48:02 -0700

    changes

--- Console.java
+++ Console.java
@@ -4,7 +4,7 @@ public class Console implements Runnable
 {
 	Scanner Console = new Scanner(System.in);
 	ServerData serverData = new ServerData();
-
+	String[] split = new String[2];
 	Console()
 	{
 
@@ -17,8 +17,17 @@ public class Console implements Runnable
 			try
 			{
 				Thread.sleep(1000);
+				split[0] = "";
+				split[1] = "";
 				String command = Console.nextLine();
 				System.out.println("echo : " + command);
+				if(command.startsWith("getuser="))
+				{
+					String value;
+					split = command.split("=");
+					value = split[1];
+					serverData.userBase.get(value);
+				}
 			}
 			catch(InterruptedException e)
 			{
---
+++ ServerInstance.java
@@ -0,0 +1,5 @@
+public class ServerInstance
+{
+	String client1Add;
+	String client2Add;
+}
