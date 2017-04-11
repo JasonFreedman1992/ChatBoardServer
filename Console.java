f002033559d94d1a31a48a10bc3f41e7029a080b
jason <jasonfreedman1992@gmail.com>
2017-04-11 15:35:27 -0700

    changes

---
+++ Console.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Console implements Runnable
+{
+	Scanner Console = new Scanner(System.in);
+	ServerData serverData = new ServerData();
+	
+	Console()
+	{
+		
+	}
+
+	public void run()
+	{
+		while(true)
+		{
+			try
+			{
+				Thread.sleep(1000);
+				String command = Console.nextLine();
+			}
+			catch(InterruptedException e)
+			{
+
+			}
+		}
+	}
+}
--- Server.java
+++ Server.java
@@ -5,12 +5,16 @@ public class Server
 	public static void main(String[] args) throws IOException
 	{
 		Thread initMasterThread = new Thread();
+		Thread initConsoleThread = new Thread();
 		ServerData serverData = new ServerData();
 
 		listen initMaster = new listen();
 		initMasterThread = new Thread(initMaster);
 		initMasterThread.start();
 
+		Console initConsole = new Console();
+		initConsoleThread = new Thread(initConsole);
+		initConsoleThread.start();
 		//serverThread = new Thread(loginListener.listen);
 	}
 }
