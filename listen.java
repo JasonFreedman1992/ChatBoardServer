3ac881f7b28402a9f33c03ff30bf8ee28635f62f
jason <jasonfreedman1992@gmail.com>
2017-04-19 15:04:38 -0700

    changes

--- listen.java
+++ listen.java
@@ -311,12 +311,12 @@ public class listen implements Runnable
 								{
 									String derp = "img";
 									msg(derp, serverData.Boards.get(i).users.get(x).socket);
-									try{Thread.sleep(1000);}catch(InterruptedException f){}
+									//try{Thread.sleep(1000);}catch(InterruptedException f){}
 									img(buffer, serverData.Boards.get(i).users.get(x).socket);
-									try{Thread.sleep(1000);}catch(InterruptedException f){}
+									//try{Thread.sleep(1000);}catch(InterruptedException f){}
 									//lastUser = new User(serverData.Boards.get(i).users.get(x).username, serverData.Boards.get(i).users.get(x).address, serverData.Boards.get(i).users.get(x).socket);
 									msg("off", serverData.Boards.get(i).users.get(x).socket);
-									try{Thread.sleep(1000);}catch(InterruptedException f){}
+									//try{Thread.sleep(1000);}catch(InterruptedException f){}
 								}
 							}
 						}
